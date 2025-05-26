19f58600973d6d4acfcaaa6a6141553a733ff7c0
Tefa Arya Gumilang <tefa.arya@digiform.co.id>
2025-05-26 22:05:19 +0700

    Change csp method

--- next.config.mjs
+++ next.config.mjs
@@ -10,15 +10,7 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value:
-              "default-src 'self' http: https: http://localhost:3000 https://primadonanco.com https://flagcdn.com; " +
-              "script-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com; " +
-              "frame-src https://www.youtube.com https://www.youtube-nocookie.com; " +
-              "child-src https://www.youtube.com https://www.youtube-nocookie.com; " +
-              "style-src 'self' 'unsafe-inline'; " +
-              "img-src 'self' https://i.ytimg.com https://flagcdn.com data: http: https:; " +
-              "media-src 'self'; " +
-              "connect-src 'self' http: https: http://localhost:3000 https://primadonanco.com;",
+            value: "default-src 'self' https://flagcdn.com https://i.ytimg.com https://www.youtube.com https://www.youtube-nocookie.com; img-src 'self' https://flagcdn.com https://i.ytimg.com data:; script-src 'self' 'unsafe-inline' https://www.youtube.com https://www.youtube-nocookie.com; style-src 'self' 'unsafe-inline'; frame-src https://www.youtube.com https://www.youtube-nocookie.com; child-src https://www.youtube.com https://www.youtube-nocookie.com;",
           },
         ],
       },
